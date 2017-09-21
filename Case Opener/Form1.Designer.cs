4afaa686db8516da7d2837120d9484a7f70d37fd
ThaisenPM <21050437+ThaisenPM@users.noreply.github.com>
2017-09-20 23:24:27 -0600

    First Push

---
+++ Case Opener/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Case_Opener
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Case";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Congrats you opened a ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "$result";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 173);
+            this.listBox1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 224);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(106, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Auto open cases";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "View";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(344, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Time";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 247);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(260, 45);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(102, 282);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(81, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Copy";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(191, 282);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(81, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Reset Stats";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 311);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 314);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Time to run (In MS)";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(167, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Tick frequency slider";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 343);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Open Case";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Case Opener/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Case_Opener
+{
+    public partial class Form1 : Form
+    {
+
+        int gen;
+        int blue;
+        int purple;
+        int pink;
+        int red;
+        int exotic;
+        int total;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            trackBar1.Maximum = 1000;
+            trackBar1.TickFrequency = 83;
+            label1.Text = "Open a case to start!";
+            label2.Text = string.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int visibleItems = listBox1.ClientSize.Height / listBox1.ItemHeight;
+            listBox1.TopIndex = Math.Max(listBox1.Items.Count - visibleItems + 1, 0);
+            label1.Text = "Congrats you opened a";
+            label3.Text = DateTime.Now.ToString();
+
+            if (textBox1.Text == String.Empty)
+            {
+                timer2.Stop();
+            }
+            else if (Convert.ToInt32(textBox1.Text) <= 0)
+            {
+                timer2.Stop();
+            }
+            else if (Convert.ToInt32(textBox1.Text) > 0)
+            {
+                timer2.Interval = Convert.ToInt32(textBox1.Text);
+                timer2.Start();
+            }
+
+            if (checkBox1.Checked == true)
+            {
+                timer1.Interval = 1;
+                timer1.Start();
+            }
+            else if (checkBox1.Checked == false)
+            {
+                Random r = new Random();
+                int rInt = r.Next(0, 10000); //for ints
+                gen = rInt;
+                total = blue + purple + pink + red + exotic;
+
+                if (gen < 7880)
+                {
+                    label2.Text = "blue!";
+                    label2.ForeColor = System.Drawing.Color.Blue;
+                    listBox1.Items.Add("[" + DateTime.Now + "] " + "Blue");
+                    blue = blue + 1;
+                }
+                else if (gen < 9500)
+                {
+                    label2.Text = "purple!";
+                    label2.ForeColor = System.Drawing.Color.Purple;
+                    listBox1.Items.Add("[" + DateTime.Now.ToString() + "] " + "Purple");
+                    purple = purple + 1;
+                }
+                else if (gen < 9750)
+                {
+                    label2.Text = "pink!";
+                    label2.ForeColor = System.Drawing.Color.Magenta;
+                    listBox1.Items.Add("[" + DateTime.Now.ToString() + "] " + "Pink");
+                    pink = pink + 1;
+                }
+                else if (gen < 9910)
+                {
+                    label2.Text = "red!";
+                    label2.ForeColor = System.Drawing.Color.Red;
+                    listBox1.Items.Add("[" + DateTime.Now.ToString() + "] " + "Red");
+                    red = red + 1;
+                }
+                else if (gen < 10000)
+                {
+                    label2.Text = "exotic!";
+                    label2.ForeColor = System.Drawing.Color.Gold;
+                    listBox1.Items.Add("[" + DateTime.Now.ToString() + "] " + "Exotic");
+                    exotic = exotic + 1;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (trackBar1.Value == 0)
+            {
+                timer1.Interval = 1;
+            }
+            else if (trackBar1.Value > 0)
+            {
+                timer1.Interval = trackBar1.Value;
+            }
+
+            int visibleItems = listBox1.ClientSize.Height / listBox1.ItemHeight;
+            listBox1.TopIndex = Math.Max(listBox1.Items.Count - visibleItems + 1, 0);
+            label3.Text = DateTime.Now.ToString();
+
+            if (checkBox1.Checked == true)
+            {
+                Random r = new Random();
+                int rInt = r.Next(0, 10000); //for ints
+                gen = rInt;
+                total = blue + purple + pink + red + exotic;
+
+                if (gen < 7880)
+                {
+                    label2.Text = "blue!";
+                    label2.ForeColor = System.Drawing.Color.Blue;
+                    listBox1.Items.Add("[" + DateTime.Now + "] " + "Blue");
+                    blue = blue + 1;
+                }
+                else if (gen < 9500)
+                {
+                    label2.Text = "purple!";
+                    label2.ForeColor = System.Drawing.Color.Purple;
+                    listBox1.Items.Add("[" + DateTime.Now.ToString() + "] " + "Purple");
+                    purple = purple + 1;
+                }
+                else if (gen < 9750)
+                {
+                    label2.Text = "pink!";
+                    label2.ForeColor = System.Drawing.Color.Magenta;
+                    listBox1.Items.Add("[" + DateTime.Now.ToString() + "] " + "Pink");
+                    pink = pink + 1;
+                }
+                else if (gen < 9910)
+                {
+                    label2.Text = "red!";
+                    label2.ForeColor = System.Drawing.Color.Red;
+                    listBox1.Items.Add("[" + DateTime.Now.ToString() + "] " + "Red");
+                    red = red + 1;
+                }
+                else if (gen < 10000)
+                {
+                    label2.Text = "exotic!";
+                    label2.ForeColor = System.Drawing.Color.Gold;
+                    listBox1.Items.Add("[" + DateTime.Now.ToString() + "] " + "Exotic");
+                    exotic = exotic + 1;
+                }
+            }
+            else if (checkBox1.Checked == false)
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tick Rate: " + trackBar1.Value + Environment.NewLine + "Blue: " + blue.ToString() + Environment.NewLine + "Purple: " + purple.ToString() + Environment.NewLine + "Pink: " + pink.ToString() + Environment.NewLine + "Red: " + red.ToString() + Environment.NewLine + "Exotic: " + exotic.ToString() + Environment.NewLine + "Total: " + total.ToString());
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Tick Rate: " + trackBar1.Value + Environment.NewLine + "Blue: " + blue.ToString() + Environment.NewLine + "Purple: " + purple.ToString() + Environment.NewLine + "Pink: " + pink.ToString() + Environment.NewLine + "Red: " + red.ToString() + Environment.NewLine + "Exotic: " + exotic.ToString() + Environment.NewLine + "Total: " + total.ToString() + Environment.NewLine + Environment.NewLine + "Stats by: Thaisen's C# Case Simulator (v1.0.0.4)");
+            MessageBox.Show("Stats copied to your clipboard");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            blue = 0;
+            purple = 0;
+            pink = 0;
+            red = 0;
+            exotic = 0;
+            total = 0;
+            listBox1.Items.Clear();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            MessageBox.Show("Complete");
+        }
+    }
+}
